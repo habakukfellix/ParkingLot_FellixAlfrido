24a803916859213ced3c61b6502ba404ca6649d1
lenovo <lenovo@192.168.0.36>
2018-08-15 19:55:48 +0700

    new commit

---
+++ src/main/java/com/mycompany/ParkingLot/data/ParkingLot.java
@@ -0,0 +1,19 @@
+package com.mycompany.ParkingLot.data;
+
+public class ParkingLot{
+	private int slotNumber;
+	private Car car;
+	ParkingLot(int slotNo, Car c){
+		this.slotNumber = slotNo;
+		this.car = c;
+	}
+	public int getSlotNumber() {
+		return slotNumber;
+	}
+	
+	public Car getCar() {
+		return car;
+	}
+
+	
+}
